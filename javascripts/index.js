d23788d1787b01cf4ca4c33c5a8a04add5b9c3a1
ShanteTorres <kennedy2110@gmail.com>
2022-01-14 17:02:36 -0500

    add basic files

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,26 +5,26 @@ const homePageLink = () => document.getElementById('home-page-link');
 const tvShowSearchLink = () => document.getElementById('tv-show-search-link');
 
     
-   function searchShow(query) {
-       const url = `https://api.tvmaze.com/search/shows?q=${query}`;
-       fetch(url)
-       .then(response => response.json())
-       .then((results) => {
-     const list = document.getElementById("resultsList"); 
-      results.forEach(result => {
-       console.log(result)
-       const body = document.getElementsByTagName("body")[0];
-       const element = document.createElement("li");
-       element.innerText = result.show.name;
-       const button = document.createElement("button");
-       button.innerHTML = "Watched";
-       list.appendChild(element);
-       element.appendChild(button);
-       button.addEventListener ("click", () => {
-        alert("You've seen this before.");
-        });
-     })
-    });
+function searchShow(query) {
+  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(url)
+  .then(response => response.json())
+  .then((results) => {
+const list = document.getElementById("resultsList"); 
+ results.forEach(result => {
+  console.log(result)
+  const body = document.getElementsByTagName("body")[0];
+  const element = document.createElement("li");
+  element.innerText = result.show.name;
+  const button = document.createElement("button");
+  button.innerHTML = "Watched";
+  list.appendChild(element);
+  element.appendChild(button);
+  button.addEventListener ("click", () => {
+   alert("You've seen this before.");
+   });
+})
+});
 }
 
 
